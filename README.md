824396818a33f52a4fed26fb364e5207af465b0a
Nitish Tewari <tansh2611@gmail.com>
2018-09-09 21:05:36 +0530

    Error handling implemented.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# assignment2
-Server API
+# Backend Application
+
+This assignment is created for technical assessment with ITH technologies!
+
+## Required Softwares
+
+NodeJs : v8.11.1
+
+npm : v6.1.0
+
+MongoDB server: v3.6
+
+## Steps for setup
+
+Take a pull of the master branch from the github. Perform `npm install` to install the required dependencies.
+
+Start the mongoDB server on its default port.
+
+## Development server
+
+Run `npm start` to start the dev server. The backend application will start running on `http://localhost:3000/`.
+
+## NOTE
+
+`mongod.exe` should be running on the machine before starting the node server.
+
+Seed data is not required for this application.
--- api/controllers/serverApiController.js
+++ api/controllers/serverApiController.js
@@ -1,30 +1,52 @@
 'use strict';
 var mongoose = require('mongoose'),
-    Task = mongoose.model('userdetails');
+  Task = mongoose.model('userdetails');
 
 exports.list_all_users = function (req, res) {
-  Task.find({}, function (err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
+  try {
+    Task.find({}, function (err, task) {
+      if (err)
+        res.json({ error: true, message: "Unexpected error occurred!" });
+
+      res.json({ error: false, data: task });
+    });
+  } catch (err) {
+    console.log("Error! : ", err.message);
+    res.json({ error: true, message: "Unexpected error occurred!" });
+  }
+
 };
 
 exports.create_a_task = function (req, res) {
   var new_task = new Task(req.body);
-  new_task.save(function (err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'User successfully added' });
-  });
+
+  try {
+    new_task.save(function (err, task) {
+      if (err)
+        res.json({ error: true, message: "Unexpected error occurred!" });
+
+      res.json({ error: false, message: 'User successfully added' });
+    });
+  } catch (err) {
+    console.log("Error! : ", err.message);
+    res.json({ error: true, message: "Unexpected error occurred!" });
+  }
+
 };
 
 exports.delete_a_task = function (req, res) {
-  Task.deleteOne({
-    _id: req.params._id
-  }, function (err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'User successfully deleted' });
-  });
+  try {
+    Task.deleteOne({
+      _id: req.params._id
+    }, function (err, task) {
+      if (err)
+        res.json({ error: true, message: "Unexpected error occurred!" });
+
+      res.json({ error: false, message: 'User successfully deleted' });
+    });
+  } catch (err) {
+    console.log("Error! : ", err.message);
+    return res.json({ error: true, message: "Unexpected error occurred!" });
+  }
+
 };
